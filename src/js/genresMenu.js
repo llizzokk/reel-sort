4262bea5a8ab854f68cf0a3d0820231f8a9cd358
Liza <lizaapostol378@gmail.com>
2024-12-22 20:49:16 +0200

    update genres menu

---
+++ src/js/genresMenu.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const dropdown = document.querySelector('.dropdown');
+const dropdownButton = document.querySelector('.dropdown-button');
+const dropdownContent = document.querySelector('.genresContainer');
+const genres = document.querySelectorAll('.genres-list-button');
+
+dropdown.addEventListener('mouseover', function () {
+  dropdownContent.style.display = 'flex';
+});
+
+dropdown.addEventListener('mouseleave', function () {
+  dropdownContent.style.display = 'none';
+});
+
+dropdownContent.addEventListener('click', function (event) {
+  if (event.target && event.target.classList.contains('genres-list-button')) {
+    dropdownButton.textContent = event.target.textContent;
+    dropdownContent.style.display = 'none';
+  }
+});
