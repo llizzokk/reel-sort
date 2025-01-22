96c7258a02ddd7de59853ae0cad89c0cf072b300
Liza <lizaapostol378@gmail.com>
2025-01-22 13:10:32 +0200

    update animations

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -4,6 +4,26 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const text = 'Leave a review...';
+  const typingElement = document.querySelector('.feedback-title');
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      typingElement.textContent = text.slice(0, index + 1);
+      index++;
+      setTimeout(type, 150);
+    } else {
+      setTimeout(() => {
+        index = 0;
+        typingElement.textContent = '';
+        type();
+      }, 2000);
+    }
+  }
+
+  type();
+
   const form = document.getElementById('feedbackForm');
   const submitButton = document.getElementById('submitButton');
   const inputs = document.querySelectorAll('.feedback-input');
